ff153604717824b2ae15f99baa2fa804000023e0
cybermyv <yuriymironov77@gmail.com>
2019-06-13 21:10:24 +0400

    [*] доработал функцию очистки поля

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-(function() {
-	'use strict';
+(function () {
+	"use strict";
 
 	const CHESS_DIMMENSION = 10;
-	const EDGING_BOARD = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H' ];
+	const EDGING_BOARD = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 	const chessBoard = document.querySelector('.chessBoard');
 
 	function clearChessBord() {
-		for (let i = 1; i < CHESS_DIMMENSION - 1; i++) {
-			for (let j = 1; j < CHESS_DIMMENSION - 1; j++) {
-				if (chessBoard.rows[i].cells[j].classList.contains('horse'))
-					chessBoard.rows[i].cells[j].classList.remove('horse');
+		const allMove = document.querySelectorAll('.move');
+		const horse = document.querySelector('.horse');
 
-				if (chessBoard.rows[i].cells[j].classList.contains('move'))
-					chessBoard.rows[i].cells[j].classList.remove('move');
-			}
+		if (horse) horse.classList.remove('horse');
+
+		if (allMove) {
+			allMove.forEach(el => {
+				el.classList.remove('move');
+			});
 		}
+
 	}
 
 	function setMove(x, y) {
@@ -68,8 +70,13 @@
 		for (let j = 0; j < CHESS_DIMMENSION; j++) {
 			const cell = row.insertCell(j);
 
-			i === 0 || i === CHESS_DIMMENSION - 1 ? cell.classList.add('edging') : cell.setAttribute('data-cell-x', i);
-			j === 0 || j === CHESS_DIMMENSION - 1 ? cell.classList.add('edging') : cell.setAttribute('data-cell-y', j);
+			i === 0 || i === CHESS_DIMMENSION - 1
+				? cell.classList.add('edging')
+				: cell.setAttribute('data-cell-x', i);
+
+			j === 0 || j === CHESS_DIMMENSION - 1
+				? cell.classList.add('edging')
+				: cell.setAttribute('data-cell-y', j);
 		}
 	}
 
@@ -78,20 +85,29 @@
 
 		if (i % 2) {
 			for (let j = 1; j < CHESS_DIMMENSION - 1; j++) {
-				j % 2 ? chessBoard.rows[CHESS_DIMMENSION - 1 - i].cells[j].classList.add('cellBlack') : '';
+				j % 2
+					? chessBoard.rows[CHESS_DIMMENSION - 1 - i].cells[j].classList.add(
+						'cellBlack'
+					)
+					: '';
 			}
 		} else {
 			for (let j = 1; j < CHESS_DIMMENSION - 1; j++) {
-				j % 2 ? '' : chessBoard.rows[CHESS_DIMMENSION - 1 - i].cells[j].classList.add('cellBlack');
+				j % 2
+					? ''
+					: chessBoard.rows[CHESS_DIMMENSION - 1 - i].cells[j].classList.add(
+						'cellBlack'
+					);
 			}
 		}
 	}
 
 	for (let i = 1; i < CHESS_DIMMENSION - 1; i++) {
-		chessBoard.rows[CHESS_DIMMENSION - 1].cells[i].innerHTML = EDGING_BOARD[i - 1];
+		chessBoard.rows[CHESS_DIMMENSION - 1].cells[i].innerHTML =
+			EDGING_BOARD[i - 1];
 	}
 
-	chessBoard.addEventListener('click', (event) => {
+	chessBoard.addEventListener('click', event => {
 		const td = event.target.closest('td');
 
 		if (td.hasAttribute('data-cell-x') && td.hasAttribute('data-cell-y')) {
